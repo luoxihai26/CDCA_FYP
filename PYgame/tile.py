4ba8f6b6b7df4f2b78f21ceff583c8ea70b8ff68
HungKaMing <112677091+HungKaMing@users.noreply.github.com>
2023-09-13 07:14:10 +0000

    modified:   PYgame/debug.py 	modified:   PYgame/level.py 	modified:   PYgame/player.py 	new file:   PYgame/settings.py 	new file:   PYgame/tile.py

--- PYgame/tile.py
+++ PYgame/tile.py
@@ -1,8 +1,8 @@
 import pygame
 from settings import *
 
-class Player(pygame.sprite.Sprite):
+class Tile(pygame.sprite.Sprite):
     def __init__(self,pos,groups):
         super().__init__(groups)
-        self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
+        self.image = pygame.image.load('../graphics/test/rock.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
