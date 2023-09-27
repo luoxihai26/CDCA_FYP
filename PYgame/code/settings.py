69f4c6afcfb6e481f24668802f0314500e78431d
SIK HOI LO <luoxihai26@gmail.com>
2023-09-27 22:25:59 +0800

    230927commit

--- PYgame/code/settings.py
+++ PYgame/code/settings.py
@@ -1,5 +1,18 @@
+import os
+
+sourceFileDir = os.path.abspath("C:/IVELWL/CDCA_FYP/PYgame")
+
 # game setup
 WIDTH = 1280
 HEIGTH = 720
 FPS = 60
 TILESIZE = 64
+
+# weapons
+weapon_data = {
+    "sword": {"cooldown": 100, "damage": 15, "graphic": sourceFileDir + "/graphics/weapons/sword/full.png"},
+    "lance": {"cooldown": 400, "damage": 30, "graphic": sourceFileDir + "/graphics/weapons/lance/full.png"},
+    "axe": {"cooldown": 300, "damage": 20, "graphic": sourceFileDir + "/graphics/weapons/axe/full.png"},
+    "rapier": {"cooldown": 50, "damage": 8, "graphic": sourceFileDir + "/graphics/weapons/rapier/full.png"},
+    "sai": {"cooldown": 80, "damage": 10, "graphic": sourceFileDir + "/graphics/weapons/sai/full.png"},
+}
