67b1d4553c77fbaf5d2fd9136b9ba6cad33adab3
SIK HOI LO <luoxihai26@gmail.com>
2023-10-03 17:42:51 +0800

    create UI and selection box

--- PYgame/code/level.py
+++ PYgame/code/level.py
@@ -6,6 +6,7 @@ from debug import debug
 from support import *
 from random import choice
 from weapon import Weapon
+from ui import UI
 
 sourceFileDir = os.path.abspath("C:/IVELWL/CDCA_FYP/PYgame")
 
@@ -25,6 +26,9 @@ class Level:
 
 		# sprite setup
 		self.create_map()
+  
+		# user interface
+		self.ui = UI()
 
 	def create_map(self):
 		layouts = {
@@ -68,8 +72,7 @@ class Level:
 		# update and draw the game
 		self.visible_sprites.custom_draw(self.player)
 		self.visible_sprites.update()
-		debug(self.player.status)
-
+		self.ui.display(self.player)
 
 class YSortCameraGroup(pygame.sprite.Group):
 	def __init__(self):
